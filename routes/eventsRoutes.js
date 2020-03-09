c8d48e8081e0fafd20b91e02dc60df77967f816b
David H <dph0718@gmail.com>
2020-03-08 21:42:49 -0600

    Adds create, update, read, delete routes for Event, User, & Party

---
+++ routes/eventsRoutes.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/createEvent', (req, res) => {
+    res.send("Response sent for Create Event")
+})
+
+router.post('/readEvent', (req, res) => {
+    res.send("Response sent for Read Event")
+})
+
+router.post('/updateEvent', (req, res) => {
+    res.send("Response sent for update Event")
+})
+
+router.post('/deleteEvent', (req, res) => {
+    res.send("Response sent for delete Event")
+})
+
+module.exports = router;
